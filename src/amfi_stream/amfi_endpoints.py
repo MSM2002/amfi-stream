5abd71f7ba2e3b0908c73f07e9022597d1c35cfa
MSM2002 <manaswi.m02@gmail.com>
2026-04-18 21:30:20 +0530

    fix: Update SCHEME_MASTER_URL with parameter
    because AMFI website requires it to return a value, otherwise it redirects to their login page.

--- src/amfi_stream/amfi_endpoints.py
+++ src/amfi_stream/amfi_endpoints.py
@@ -1,3 +1,3 @@
-SCHEME_MASTER_URL = "https://portal.amfiindia.com/DownloadSchemeData_Po.aspx"
+SCHEME_MASTER_URL = "https://portal.amfiindia.com/DownloadSchemeData_Po.aspx?mf=0"
 LATEST_NAV_URL = "https://portal.amfiindia.com/spages/NAVAll.txt"
 HISTORICAL_NAV_URL = "https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx"
