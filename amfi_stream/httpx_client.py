682e69164eeae15e341953ab668c3bb0e8d654c7
MSM2002 <manaswi.m02@gmail.com>
2026-04-19 12:58:12 +0530

    fix: Seperate line stripping from streaming.

--- amfi_stream/httpx_client.py
+++ amfi_stream/httpx_client.py
@@ -12,10 +12,7 @@ class HttpStreamClient:
     ) -> Iterator[str]:
         with self._client.stream("GET", url, params=params) as resp:
             for line in resp.iter_lines():
-                if not line or not line.strip():
-                    continue
-
                 if isinstance(line, bytes):
                     line = line.decode("utf-8")
 
-                yield line.strip()
+                yield line
