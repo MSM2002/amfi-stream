a2e0be5116223460a928276eacaf811e39aeaf8e
MSM2002 <manaswi.m02@gmail.com>
2026-04-26 20:28:20 +0530

    feat: Add generic normalisation and scheme master schema.

--- amfi_stream/normalisers.py
+++ amfi_stream/normalisers.py
@@ -1,17 +1,98 @@
 import pyarrow as pa
 import pyarrow.compute as pc
 
+NULL_VARIANTS = ["", "-"]
+
+SCHEME_MASTER_SCHEMA = pa.schema(
+    [
+        ("AMC", pa.string()),
+        ("Code", pa.uint32()),
+        ("Scheme Name", pa.string()),
+        ("Scheme Type", pa.string()),
+        ("Scheme Category", pa.string()),
+        ("Scheme NAV Name", pa.string()),
+        ("Scheme Minimum Amount", pa.string()),
+        ("Launch Date", pa.date32()),
+        ("Closure Date", pa.date32()),
+        ("ISIN Div Payout/ ISIN GrowthISIN Div Reinvestment", pa.string()),
+    ]
+)
+
+MONTH_KEYS = pa.array(
+    [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sep",
+        "Oct",
+        "Nov",
+        "Dec",
+    ]
+)
+
+MONTH_VALUES = pa.array(
+    ["01", "02", "03", "04", "05", "06", "07", "08", "09", "10", "11", "12"]
+)
+
+
+def apply_null(col: pa.Array) -> pa.Array:
+    mask = pc.is_in(col, pa.array(NULL_VARIANTS))
+    return pc.if_else(mask, pa.scalar(None, type=col.type), col)
+
+
+def parse_dmy_date(col: pa.Array) -> pa.Array:
+    parts = pc.split_pattern(col, "-")
+
+    day = pc.list_element(parts, 0)
+    month = pc.list_element(parts, 1)
+    year = pc.list_element(parts, 2)
+
+    index = pc.index_in(month, value_set=MONTH_KEYS)
+    month_number = pc.take(MONTH_VALUES, index)
+
+    iso_date_string = pc.binary_join_element_wise(
+        year, pc.binary_join_element_wise(month_number, day, "-"), "-"
+    )
+
+    return pc.strptime(iso_date_string, format="%Y-%m-%d", unit="s")
+
+
+def cast_to(col, dtype):
+    return pc.cast(col, dtype)
+
+
+def normalise_column(col, field):
+    if pa.types.is_string(col.type):
+        col = pc.utf8_trim_whitespace(pc.cast(col, pa.string()))
+        col = apply_null(col)
+
+    if pa.types.is_date(field.type):
+        col = parse_dmy_date(col)
+
+    col = cast_to(col, field.type)
+
+    return col
+
+
+def trim_table_column_names(table: pa.Table) -> pa.Table:
+    return table.rename_columns([name.strip() for name in table.column_names])
+
+
+def normalise_table(table, schema):
+    table = trim_table_column_names(table)
+    arrays = []
+    for field in schema:
+        col = table[field.name]
+        col = normalise_column(col, field)
+        arrays.append(col)
+    return pa.Table.from_arrays(arrays, schema=schema)
+
 
 def normalise_scheme_master(table: pa.Table) -> pa.Table:
-    cols = table.column_names
-    new_cols = []
-
-    for col in cols:
-        arr = table[col]
-
-        if pa.types.is_string(arr.type):
-            arr = pc.utf8_trim_whitespace(arr)
-
-        new_cols.append(arr)
-
-    return pa.Table.from_arrays(new_cols, names=cols)
+    table = normalise_table(table, SCHEME_MASTER_SCHEMA)
+    return table
