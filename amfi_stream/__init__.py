daa475b0b84f4668bed7dbc438c506eed2a06330
MSM2002 <manaswi.m02@gmail.com>
2026-04-26 21:11:20 +0530

    chore: Create __init__.py.

---
+++ amfi_stream/__init__.py
@@ -0,0 +1,6 @@
+from amfi_stream.factories import stream_latest_nav, stream_scheme_master
+from amfi_stream.pipeline import AMFIPipeline
+
+__all__ = ["AMFIPipeline", "stream_latest_nav", "stream_scheme_master"]
+
+__version__ = "0.1.0"
