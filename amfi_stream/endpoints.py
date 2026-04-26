cea20b485fd51918184bc9a0bfb28d076c8c14b3
MSM2002 <manaswi.m02@gmail.com>
2026-04-26 14:36:06 +0530

    feat: Make endpoints callable.

---
+++ amfi_stream/endpoints.py
@@ -0,0 +1,21 @@
+from datetime import date
+
+SCHEME_MASTER_URL = "https://portal.amfiindia.com/DownloadSchemeData_Po.aspx?mf=0"
+LATEST_NAV_URL = "https://portal.amfiindia.com/spages/NAVAll.txt"
+HISTORICAL_NAV_URL = "https://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx"
+
+
+def scheme_master_url() -> str:
+    return SCHEME_MASTER_URL
+
+
+def latest_nav_url() -> str:
+    return LATEST_NAV_URL
+
+
+def nav_history_url(from_date: date, to_date: date) -> str:
+    return (
+        f"{HISTORICAL_NAV_URL}"
+        f"?fromDate={from_date.strftime('%d-%b-%Y')}"
+        f"&toDate={to_date.strftime('%d-%b-%Y')}"
+    )
