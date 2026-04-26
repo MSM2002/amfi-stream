cfa0b8f08e8937cdad551442ce4bd4ea8b750739
MSM2002 <manaswi.m02@gmail.com>
2026-04-26 21:04:43 +0530

    feat: Add job instance for latest_nav

--- amfi_stream/factories.py
+++ amfi_stream/factories.py
@@ -1,8 +1,8 @@
 from datetime import date
 
-from amfi_stream.endpoints import scheme_master_url
+from amfi_stream.endpoints import latest_nav_url, scheme_master_url
 from amfi_stream.models import AMFIJob
-from amfi_stream.normalisers import normalise_scheme_master
+from amfi_stream.normalisers import normalise_latest_nav, normalise_scheme_master
 from amfi_stream.sources import StaticURLSource
 
 
@@ -16,7 +16,14 @@ def stream_scheme_master() -> AMFIJob:
     )
 
 
-def stream_latest_nav() -> AMFIJob: ...
+def stream_latest_nav() -> AMFIJob:
+    return AMFIJob(
+        name="latest_nav",
+        urls_source=StaticURLSource(latest_nav_url()),
+        response_delimiter=";",
+        response_col_count=6,
+        normaliser=normalise_latest_nav,
+    )
 
 
-def stream_nav_history(from_date: date, to_date: date) -> AMFIJob: ...
+def stream_nav_histor(from_date: date, to_date: date) -> AMFIJob: ...
