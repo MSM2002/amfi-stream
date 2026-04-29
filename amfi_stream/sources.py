f15cc245c3295e938499b5f158a286c13aa5cf53
MSM2002 <manaswi.m02@gmail.com>
2026-04-29 22:15:33 +0530

    feat: Add date chunking function for historical NAV.

--- amfi_stream/sources.py
+++ amfi_stream/sources.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 from typing import Protocol
 
 from amfi_stream.endpoints import historical_nav_url
@@ -17,14 +17,27 @@ class StaticURLSource:
 
 
 class HistoricalNAVSource:
-    def __init__(self, from_date: date, to_date: date | None = None):
+    MAX_DAY_RANGE_FOR_HISTORICAL_NAV_RESPONSE = 90
+
+    def __init__(self, from_date: date, to_date: date):
         self.from_date = from_date
-        self.to_date = to_date or from_date
+        self.to_date = to_date
 
     def __call__(self) -> list[str]:
         urls = []
-        for f, t in self.chunker():
+        for f, t in self._date_chunker():
             urls.append(historical_nav_url(f, t))
         return urls
 
-    def chunker(self): ...
+    def _date_chunker(self) -> list[tuple[date, date]]:
+        date_chunks = []
+        current_start_date = self.from_date
+        end_date = self.to_date
+        while current_start_date <= end_date:
+            current_end_date = current_start_date + timedelta(
+                days=self.MAX_DAY_RANGE_FOR_HISTORICAL_NAV_RESPONSE - 1
+            )
+            actual_end_date = min(current_end_date, end_date)
+            date_chunks.append((current_start_date, actual_end_date))
+            current_start_date = actual_end_date + timedelta(days=1)
+        return date_chunks
