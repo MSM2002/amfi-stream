8774d16b0b3d27039be509e4b4559fa9dfcbb659
MSM2002 <manaswi.m02@gmail.com>
2026-05-01 22:27:32 +0530

    build: Bump version number and add historical nav to the public api.

--- amfi_stream/__init__.py
+++ amfi_stream/__init__.py
@@ -1,6 +1,15 @@
-from amfi_stream.factories import stream_latest_nav, stream_scheme_master
+from amfi_stream.factories import (
+    stream_historical_nav,
+    stream_latest_nav,
+    stream_scheme_master,
+)
 from amfi_stream.pipeline import AMFIPipeline
 
-__all__ = ["AMFIPipeline", "stream_latest_nav", "stream_scheme_master"]
+__all__ = [
+    "AMFIPipeline",
+    "stream_latest_nav",
+    "stream_scheme_master",
+    "stream_historical_nav",
+]
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
